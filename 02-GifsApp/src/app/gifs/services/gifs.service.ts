50777303402bd2eb577c404c95d969f777c92d45
William Santos <william.santos.satalaya@outlook.com>
2022-10-10 10:22:22 -0500

    Colocando un tipado a las peticiones http

--- 02-GifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-GifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class GifsService {
   private _historial: string[] = [];
 
   //TODO: Cambiar any por su tipo correspondiente
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial(){
     return [...this._historial];
@@ -22,16 +23,16 @@ export class GifsService {
 
     query = query.trim().toLocaleLowerCase();
 
-    if(!this._historial.includes( query )){      
-      
-      this._historial.splice(0,10);      
+    if(!this._historial.includes( query )){       
       this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);  
+      
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=Fm2uHH4HHizQiB2ZfxVjk1wHjUsIAL4h&q=dragon${ query }&limit=10`)
-        .subscribe( (resp: any) => {
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=Fm2uHH4HHizQiB2ZfxVjk1wHjUsIAL4h&q=dragon${ query }&limit=10`)
+        .subscribe( (resp: SearchGifsResponse) => {
           console.log( resp.data)
-          this.resultados = resp.data;
+          this.resultados = resp.data;          
         })   
   }
 
