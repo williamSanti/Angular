4ed6035cc9b695a8a156b1ea70a4440c782e1a6f
William Santos <william.santos.satalaya@outlook.com>
2022-10-09 23:30:30 -0500

    GifsModule y sus conponentes

---
+++ 02-GifsApp/src/app/gifs/resultado/resultado.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resultado',
+  templateUrl: './resultado.component.html'
+})
+export class ResultadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
