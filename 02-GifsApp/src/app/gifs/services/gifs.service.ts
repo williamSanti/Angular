e4169571408950c7c372d92e1755d218effb051b
Willan.Santos <willan.santos@neoris.com>
2022-10-13 11:36:37 -0300

    AnimateStyleCSS - Fin  del proyecto

--- 02-GifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-GifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { ThisReceiver } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGifsResponse } from '../interfaces/gifs.interfaces';
@@ -9,6 +9,7 @@ import { Gif, SearchGifsResponse } from '../interfaces/gifs.interfaces';
 export class GifsService {
 
   private apiKey: string ='Fm2uHH4HHizQiB2ZfxVjk1wHjUsIAL4h'
+  private servicioUrl: string ='https://api.giphy.com/v1/gifs'
   private _historial: string[] = [];
 
   //TODO: Cambiar any por su tipo correspondiente
@@ -31,13 +32,18 @@ export class GifsService {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10); 
       
-      localStorage.setItem('hisSidebar', JSON.stringify( this._historial ));
-      
+      localStorage.setItem('hisSidebar', JSON.stringify( this._historial ));      
     }
 
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=Fm2uHH4HHizQiB2ZfxVjk1wHjUsIAL4h&q=dragon${ query }&limit=10`)
+    const params = new HttpParams()
+            .set('apikey', this.apiKey)
+            .set('limit', 10)
+            .set('q', query)
+
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`, { params: params})
         .subscribe( (resp: SearchGifsResponse) => {
-          console.log( resp.data)
+          //console.log( resp.data)
           this.resultados = resp.data;         
 
           localStorage.setItem( 'hisImagenes', JSON.stringify( this.resultados ));
