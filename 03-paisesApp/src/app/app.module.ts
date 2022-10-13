f4fc7d786548dda9bc1f9af5fc5acccbef692046
Willan.Santos <willan.santos@neoris.com>
2022-10-13 17:13:49 -0300

    RouterModule, rutas en la app - RouterLink

--- 03-paisesApp/src/app/app.module.ts
+++ 03-paisesApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
@@ -11,6 +12,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     SharedModule,
     PaisModule
   ],
